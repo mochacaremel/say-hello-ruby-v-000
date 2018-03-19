6ad5580189368e8f0a4e89d66de81cb535ba9579
Asheley <asheleymcdonald@gmail.com>
2018-03-19 00:49:46 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,6 +1,6 @@
-require_relative './spec_helper'
+require_relative '../spec_helper'
 
-describe "say_hello.rb" do 
+describe "say_hello.rb" do
 
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
