ab6596caed49294d125980a835dd9582e57a0ca1
Asheley <asheleymcdonald@gmail.com>
2018-03-19 00:49:06 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
 # Build your say_hello method here
 describe "say_hello" do
-
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
